ca04eebfe96ec10ebe5cf617091de3e300e95743
Alexey Bukin <alexbukin90@gmail.com>
2022-11-25 21:24:28 -0500

    Oracle DB and SQL driver dependencies added to pom.xml

--- src/test/java/com/library/runner/CukesRunner.java
+++ src/test/java/com/library/runner/CukesRunner.java
@@ -16,7 +16,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features" ,
         glue = "com/trycloud/test/step_definition",
         dryRun = false,
-        tags = "@US2",
+        tags = "",
         publish = true
 )
 public class CukesRunner {
